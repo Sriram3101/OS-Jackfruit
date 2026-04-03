6e63807de2245af43a61517ba8aa27e0c39ed62c
Sriram3101 <ksriram3101@gmail.com>
2026-04-03 09:16:29 +0530

    Add TODO for supervisor container metadata response
    Add TODO comment for supervisor response with metadata.

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -838,6 +838,7 @@ static int cmd_ps(void)
      * The supervisor should respond with container metadata.
      * Keep the rendering format simple enough for demos and debugging.
      */
+	
 	return send_control_request(&req);
 	
 }
